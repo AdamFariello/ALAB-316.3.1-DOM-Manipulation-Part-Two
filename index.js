b4f6aa68d3ee805e3ea3770f3b5569ee36ce114d
AdamFariello <ajf270@scarletmail.rutgers.edu>
2025-08-14 09:44:26 -0400

    Did part 1 and 2

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-//TODO: Figure out why this line of code doesn't work
-//      (Maybe because this only works with vue?)
-//import "styles.css";
-
+/*Original part 1 code*/
 // Menu data structure
 var menuLinks = [
     { text: 'about', href: '/about' },
@@ -10,25 +7,23 @@ var menuLinks = [
     { text: 'account', href: '/account' },
 ];
 
-
 // Part 1
 let mainEl = document.querySelector("main")
 mainEl.setAttribute("style", "background-color:var(--main-bg)")
 mainEl.innerHTML = "<h1>DOM MANIPULATION</h1>"
 mainEl.setAttribute("class", "flex-ctr")
 
-
 //Part 2
-//TODO: figure out why I can't see bar
 let topMenuEl = document.getElementById("top-menu")
-// /mainEl.insertBefore(topMenuEl, mainEl.firstChild)
-topMenuEl.setAttribute("height", "100%") //TODO: change to use style
+topMenuEl.setAttribute("height", "100%") 
 topMenuEl.setAttribute("style", "background-color:var(--top-menu-bg)")
 topMenuEl.setAttribute("class", "flex-around")
-
 
 //part 3
 for (menu of menuLinks) {
     let a = `<a href=${menu.href}>${menu.text.toUpperCase()}</a>`
     topMenuEl.innerHTML += a
 }
+
+
+/*New part 2 code*/
